9f72e2b3cdcdfe8f9929db2cc4820f55ab0b70df
chyler <chyler@chyler.myguest.virtualbox.org>
2024-03-31 23:53:35 -0400

    PHP Ch 3-4

---
+++ payCalculator.php
@@ -0,0 +1,66 @@
+<?php 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+//hard coded vars
+$employeeName = "Chyler Espino"; //employee name
+$hoursWorked = 40.0; //hrs worked in week
+$payRate = 54.50; //pay rate by hr 
+$federalTaxRate = 0.245; //fed tax witholding rate
+$stateTaxRate = 0.055; //state tax witholding rate
+
+//calculations
+$grossPay = $hoursWorked * $payRate;
+$federalWithholding = $grossPay * $federalTaxRate;
+$stateWithholding = $grossPay * $stateTaxRate;
+$totalDeductions = $federalWithholding + $stateWithholding;
+$netPay = $grossPay - $totalDeductions;
+
+$annualPay = $grossPay * 52; //weeks in a year
+
+//number formatting
+$hoursWorked = number_format($hoursWorked, 2);
+$payRate = number_format($payRate, 2);
+$grossPay = number_format($grossPay, 2);
+$federalWithholding = number_format($federalWithholding, 2);
+$stateWithholding = number_format($stateWithholding, 2);
+$totalDeductions = number_format($totalDeductions, 2);
+$netPay = number_format($netPay, 2);
+
+//federal tax bracket
+$federalTaxBracket = "";
+if ($annualPay <= 11600) {
+	$federalTaxBracket = "10%";
+}
+elseif($annualPay >= 11601 && $annualPay <= 47150) {
+	$federalTaxBracket = "12%";
+}
+elseif($annualPay >= 47151 && $annualPay <= 100525) {
+	$federalTaxBracket = "22%";
+}
+elseif($annualPay >= 100526 && $annualPay <= 191950) {
+	$federalTaxBracket = "24%";
+}
+elseif($annualPay >= 191951 && $annualPay <= 243725) {
+	$federalTaxBracket = "32%";
+}
+elseif($annualPay >= 243726 && $annualPay <= 609350) {
+	$federalTaxBracket = "35%";
+}
+else {
+	$federalTaxBracket = "37%";
+}
+
+//output 
+echo "<table>";
+echo "<tr><th>Employee Name</th><td>$employeeName</td></tr>";
+echo "<tr><th>Hours Worked</th><td>$hoursWorked</td></tr>";
+echo "<tr><th>Pay Rate</th><td>\$$payRate</td></tr>";
+echo "<tr><th>Gross Pay</th><td>\$$grossPay</td></tr>";
+echo "<tr><th>Federal Withholding (24.5%)</th><td>\$$federalWithholding</td></tr>";
+echo "<tr><th>State Withholding (5.5%)</th><td>\$$stateWithholding</td></tr>";
+echo "<tr><th>Total Deductions</th><td>\$$totalDeductions</td></tr>"
+echo "<tr><th>Net Pay</th><td>\$$netPay</td></tr>";
+echo "<tr><th>Federal Tax Bracket</th><td>\$$federalTaxBracket</td></tr>";
+?>
+
